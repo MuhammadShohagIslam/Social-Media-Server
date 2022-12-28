a3fe22f0ce8ea7771a076e73a687fdecc32e1e47
MuhummadShohag <muhammadshohagislam.software.e@gmail.com>
2022-12-28 07:45:15 +0300

    project setup

--- index.js
+++ index.js
@@ -1,8 +1,38 @@
-const { MongoClient, ServerApiVersion } = require('mongodb');
-const uri = "";
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-client.connect(err => {
-  const postsCollection = client.db("socialMedia").collection("posts");
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
 
-  client.close();
+const app = express();
+const port = process.env.PORT || 5000;
+
+// middleware
+app.use([cors(), express.json()]);
+
+// connect mongoDB
+const uri = process.env.MONGO_URI;
+
+const client = new MongoClient(uri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    serverApi: ServerApiVersion.v1,
+});
+
+const run = async () => {
+    try {
+        const postsCollection = client.db("socialMedia").collection("posts");
+
+        app.get("/", (req, res)=>{
+            res.json("A")
+        })
+
+    } finally{
+
+    }
+}
+
+run().catch(error=> console.log(error.message));
+
+app.listen(port, () => {
+    console.log(`ShohagCSM server is running on ${port}`);
 });
